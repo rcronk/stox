8740a0f2dbba26cc6ba1932cce2ea94aa2edb06f
rcronk <cronk.r@gmail.com>
2016-04-25 23:45:57 -0600

    Fixing non-standard indentation.

--- test/test_stox.py
+++ test/test_stox.py
@@ -5,8 +5,8 @@ import stox.stox
 
 class TestCompany(unittest.TestCase):
 
-  def test_get_stock_price(self):
-      self.assertEqual(stox.stox.Company('goog').get_stock_price(), 9.99)
+    def test_get_stock_price(self):
+        self.assertEqual(stox.stox.Company('goog').get_stock_price(), 9.99)
 
 
 if __name__ == '__main__':
